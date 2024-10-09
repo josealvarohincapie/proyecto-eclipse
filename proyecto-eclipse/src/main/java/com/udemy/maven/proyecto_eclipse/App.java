219614b6655a4b9adc764fa26583b1ed92dc4d44
José Álvaro Hincapié Castillo <136629530+josehincapie@users.noreply.github.com>
2024-10-09 13:19:37 -0500

    curso udemy - maven

---
+++ proyecto-eclipse/src/main/java/com/udemy/maven/proyecto_eclipse/App.java
@@ -0,0 +1,10 @@
+package com.udemy.maven.proyecto_eclipse;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
